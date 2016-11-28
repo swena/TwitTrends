000a5ab99fad7a8cf810577b4c20698b21f4ce2e
swena <gupta.swena@gmail.com>
2016-11-28 14:22:51 -0500

    Update snsconfirm.php
    Update

--- snsconfirm.php
+++ snsconfirm.php
@@ -2,6 +2,11 @@
 require 'vendor/autoload.php';
 use Aws\Sns\SnsClient; 
 $headers = apache_request_headers();
+$params = array();
+$params['hosts'] = array (
+	'http://localhost'        // SSL to localhost
+);
+$es = new Elasticsearch\Client($params);
 
     foreach ($headers as $header => $value) {
         echo "$header: $value <br />\n";
@@ -14,9 +19,8 @@ $client = SnsClient::factory(array(
                         'secret' => 'XXXXXX'
                     ]
     )); 
-while(true)
-{
-	if(isset($_SERVER['x-amz-sns-message-type']) && $_SERVER['x-amz-sns-message-type'] == 'SubscriptionConfirmation')
+
+if(isset($headers['x-amz-sns-message-type']) && $headers['x-amz-sns-message-type'] == 'SubscriptionConfirmation')
 	{
 		$postBody = file_get_contents('php://input');
 
@@ -28,9 +32,20 @@ while(true)
 	                'TopicArn' => 'arn:aws:sns:us-west-2:174840842210:TwittTrends', // REQUIRED
 	        ]);
 	}
-	else if (isset($_SERVER['x-amz-sns-message-type']) && $_SERVER['x-amz-sns-message-type'] == 'Notification')
+	else if (isset($headers['x-amz-sns-message-type']) && $headers['x-amz-sns-message-type'] == 'Notification')
 	{
-		print "ABCHJSKJA";
+		$postBody = file_get_contents('php://input');
+
+		// JSON decode the body to an array of message data
+		$message = json_decode($postBody, true);
+
+		$msg = json_decode($message['Message'],true);
+		$indexed = $es->index([
+                                'index' => 'twittrends',
+                                'type' => 'tweet',
+                                'body' => $msg
+                            ]);
+		//print_r($message);
 	}
-}
+
 ?>
